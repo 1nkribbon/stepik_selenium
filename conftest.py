fc4ebfa30780f5c09686e150d338e7fd3d44133c
1nkribbon <seregio@rambler.ru>
2020-11-17 15:15:29 +0300

    17112020

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
 	parser.addoption('--browser_name', action='store',
@@ -16,6 +18,8 @@ def browser(request):
 		options = webdriver.ChromeOptions()
 		options.add_experimental_option("excludeSwitches", 
 			["enable-logging"])
+		options.add_experimental_option('prefs', 
+			{'intl.accept_languages': user_language})
 		browser = webdriver.Chrome(options=options)
 	elif browser_name == "firefox":
 		print("\nstart firefox browser for test..")
